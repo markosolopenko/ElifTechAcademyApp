c6f669b6d9f496829b65b9f30105ec008a99eb2b
marko <kauchuk1616@gmail.com>
2021-02-19 14:23:13 +0200

    main page done

--- client/src/components/MainPageBody/MainPageBody.js
+++ client/src/components/MainPageBody/MainPageBody.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './mainPageBody.css'
+import { Link } from 'react-router-dom';
 
 const MainPageBody = () => {
     return (
         <div className="mainPageBody">
-            
+            <div className="slogon">
+                <div className="welcome">
+                    <div className="welcomeStart">Welcome to</div>
+                    <div className="logo">ElifTech</div>
+                    <div className="welcomeEnd">banks website</div>
+                </div>
+                <div className="opportunities">
+                    <div className="opportunities-text">With us you can easy
+                        <span className="colon">:</span></div>
+                    <div className="opportunitie">
+                        <span className="dash">-</span> 
+                        conveniently create banks 
+                    </div>
+                    <div className="opportunitie">
+                        <span className="dash">-</span> 
+                        calculate mortgage payments
+                    </div>
+                </div>   
+            </div>
+            <Link to="/banks" className="link">
+                <div className="buttonCreateBank">
+                    <button className="createBank">Create Bank</button>
+                </div>
+            </Link>
         </div>
     )
 }
--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import "./mainPage.css"
+import MainPageBody from '../../components/MainPageBody/MainPageBody';
 
 
 const Main = () => {
     return (
         <div className='mainPage'>
             <Header />
+            <MainPageBody />
         </div>
     )
 }
