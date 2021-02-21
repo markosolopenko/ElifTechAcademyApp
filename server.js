159f3d704bbb1f8e7880efabea1f84026b95037a
marko <kauchuk1616@gmail.com>
2021-02-21 19:03:52 +0200

    mern updates

--- server.js
+++ server.js
@@ -26,8 +26,11 @@ app.listen(process.env.PORT || 3002, () => {
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
 }
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-})
+
